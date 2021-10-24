6ef653ffb0921c6d532c5ee54b6b67b08c6cdb7b
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-10-24 16:00:15 +0800

    This calculator can perform exponentiation.

---
+++ exponent.py
@@ -0,0 +1,5 @@
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter the power or exponent: "))
+
+answer = num1**num2
+print(f"{answer} is the answer when {num1} is raised to {num2}.")
